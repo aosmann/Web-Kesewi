8cc28f5c3da180771d1107cca156c2273fecf98e
osman <aosmannur@gmail.com>
2023-03-25 21:13:04 +0200

    updTE

---
+++ fade.js
@@ -0,0 +1,25 @@
+
+
+
+/*$(window).scroll(function () {
+  $(".fade").css("opacity", 0 + $(window).scrollTop() / 500);
+});
+*/
+
+/*win.scroll(function(){
+scrollPosition = win.scrollTop();
+scrollRatio = 1 - scrollPosition / 300;
+$(".fade").css("opacity", scrollRatio);
+*/
+
+
+
+
+
+/*$(window).scroll(function(){
+  var scrollVar = $(window).scrollTop();
+  $('.fade').css("opacity", 1 - scrollVar/300);
+})*/
+
+
+
