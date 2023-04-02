8c75e6b75ead5d1788273a6157df717ddcfc50d0
osman <aosmannur@gmail.com>
2023-04-03 02:52:42 +0300

    asd

---
+++ submit.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  // send email to appropriate address
+  $to = "osman@kesewi.com";
+  $subject = "New message from contact form";
+  $body = "Name: $name\nEmail: $email\nMessage: $message";
+  $headers = "From: $email";
+
+  if (mail($to, $subject, $body, $headers)) {
+    echo "Thank you for your message!";
+  } else {
+    echo "Error sending message.";
+  }
+}
+?>
